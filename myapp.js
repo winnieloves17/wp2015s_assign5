68e19dc22eec07409cb57b78b02b30f95de8a5a4
Winnie <winnie@WinniedeMacBook-Pro.local>
2015-05-23 12:41:51 +0800

    6

--- myapp.js
+++ myapp.js
@@ -77,22 +77,114 @@ var cityArr = [ '臺北市',
         date3 = weatherInfo.item.forecast[3].date,
         high3 = weatherInfo.item.forecast[3].high,
         low3  = weatherInfo.item.forecast[3].low,
-        text3 = weatherInfo.item.forecast[3].text;
+        text3 = weatherInfo.item.forecast[3].text,
 
+        codelist =[weatherInfo.item.forecast[0].code,weatherInfo.item.forecast[1].code,weatherInfo.item.forecast[2].code,weatherInfo.item.forecast[3].code];
+
+        //回傳 onClick, find("name or num")
          $('.date').text(date0);
          $('#dateDay1').text(date1);
          $('#dateDay2').text(date2);
          $('#dateDay3').text(date3);
-
+  
          $('.temperature').text(low0+"-"+high0);
          $('#tempDay1').text(low1+"-"+high1);
          $('#tempDay2').text(low2+"-"+high2);
          $('#tempDay3').text(low3+"-"+high3);
 
+        //設定Skykons對應的API資料
+         for (var i=0;i<code.length;i+=1){
+          var code = codelist[i],
+              dayArr =["today","day1","day2","day3"];
+         switch(code)
+         {
+          case'1':  //tropical storm
+          case'2':  //hurricane
+          case'3':  //severe thunderstorms
+          case'4':  //thunderstorms
+          case'8':  //freezing drizzle
+          case'9':  //drizzle
+          case'10':  //freezing rain
+          case'11':  //showers
+          case'12':  //showers
+          case'37':  //isolated thunderstorms
+          case'38':  //scattered thunderstorms
+          case'39':  //scattered thunderstorms
+          case'40':  //scattered showers
+          case'45':  //thundershowers
+          case'46':  //snow showers
+          case'47':  //isolated thundershowers
+        skycons.set(dayArr[i], Skycons.Skycons.RAIN);
+
+          case'13':  //snow flurries
+          case'14':  //light snow showers
+          case'15':  //blowing snow
+          case'16':  //snow
+          case'41':  //heavy snow
+          case'42':  //scattered snow showers
+          case'43':  //heavy snow
+        skycons.set(dayArr[i], Skycons.Skycons.SNOW);
+
+          case'17':  //hail
+          case'18':  //sleet
+        skycons.set(dayArr[i], Skycons.Skycons.SLEET);
+
+          case'19':  //dust
+          case'20':  //foggy
+          case'21':  //haze
+          case'22':  //smoky
+        skycons.set(dayArr[i], Skycons.Skycons.FOG);
+
+          case'23':  //blustery
+          case'24':  //windy
+        skycons.set(dayArr[i], Skycons.Skycons.WIND);
+
+          case'25':  //cold
+          case'26':  //cloudy
+        skycons.set(dayArr[i], Skycons.Skycons.CLOUDY);
+
+          case'27':  //mostly cloudy (night)
+          case'29':  //partly cloudy (night)
+        skycons.set(dayArr[i], Skycons.Skycons.PARTLY_CLOUDY_NIGHT );
+
+          case'28':  //mostly cloudy (day)
+          case'30':  //partly cloudy (day)
+          case'44':  //partly cloudy
+        skycons.set(dayArr[i], Skycons.Skycons.PARTLY_CLOUDY_DAY);
+
+          case'31':  //clear (night)
+          case'33':  //fair (night)
+        skycons.set(dayArr[i], Skycons.Skycons.CLEAR_NIGHT);
+
+          case'32':  //sunny
+          case'34':  //fair (day)
+          case'36':  //hot
+        skycons.set(dayArr[i], Skycons.Skycons.CLEAR_DAY);
+
+          break;
+
+         }}
+
          }
        });
 
-       //回傳 onClick, find("name or num")
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
